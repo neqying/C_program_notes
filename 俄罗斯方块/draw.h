fb36bff2b17060afbeda44a8c7aa50e52fd8e8fe
David <2025225585@qq.com>
2024-03-26 17:24:15 +0800

    封面框架dw

---
+++ 俄罗斯方块/draw.h
@@ -0,0 +1,28 @@
+#pragma once 
+
+/*
+    ui:layout
+    dw:draw
+    gm:logic
+    ut:utils
+    tc:terminal control
+*/
+/*
+    	0	1	2	3	4	5	6	7	8	9	A	B	C	D	E	F
+U+250x	─	━	│	┃	┄	┅	┆	┇	┈	┉	┊	┋	┌	┍	┎	┏
+U+251x	┐	┑	┒	┓	└	┕	┖	┗	┘	┙	┚	┛	├	┝	┞	┟
+U+252x	┠	┡	┢	┣	┤	┥	┦	┧	┨	┩	┪	┫	┬	┭	┮	┯
+U+253x	┰	┱	┲	┳	┴	┵	┶	┷	┸	┹	┺	┻	┼	┽	┾	┿
+U+254x	╀	╁	╂	╃	╄	╅	╆	╇	╈	╉	╊	╋	╌	╍	╎	╏
+U+255x	═	║	╒	╓	╔	╕	╖	╗	╘	╙	╚	╛	╜	╝	╞	╟
+U+256x	╠	╡	╢	╣	╤	╥	╦	╧	╨	╩	╪	╫	╬	╭	╮	╯
+U+257x	╰	╱	╲	╳	╴	╵	╶	╷	╸	╹	╺	╻	╼	╽	╾	╿
+
+*/
+namespace dw
+{
+    void window(int top,int left,int width,int height,std::string title)
+    {
+
+    }
+}
